ac3c43423e49eb66c56c0dbe539279e00a2b3c67
AmanVerma0047 <vaman0183@gmail.com>
2024-09-18 21:16:08 +0530

    added files

---
+++ Home/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path,include
+from Home import views
+
+urlpatterns = [
+    path("",views.index,name='Home'),
+    path("about",views.about,name = 'about'),
+    path("projects",views.projects,name = 'projects'),
+    path("contactus",views.contactus,name = 'contactus'),
+    path("login",views.loginUser,name="login"),
+    path("logout",views.logoutUser,name="logout"),
+    path("profile",views.profile,name="profile"),
+]
---
+++ Home/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib import messages
+from datetime import datetime
+from Home.models import Contact
+from django.contrib.auth.models import User
+from django.contrib.auth import logout,authenticate,login
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+    #return HttpResponse("this is homepage")
+def about(request):
+    return render(request,'about.html')
+    # return HttpResponse("this is about us")
+
+def projects(request):
+    return HttpResponse("This is projects file")
+
+def contactus(request):
+    if request.method =="POST":
+        email = request.POST.get('email')
+        desc = request.POST.get('desc')
+        contact = Contact(email = email,desc=desc,date=datetime.today())
+        contact.save()
+        messages.success(request, " Thanks you for your Message!")
+
+    context = {
+        'alerttype':'success'
+    }
+    return render(request,'contactus.html',context)
+
+def loginUser(request):
+    if request.method =="POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+        # A backend authenticated the credentials
+            login(request,user)
+            return redirect("/")
+        else:
+        # No backend authenticated the credentials
+            context = {
+                'alerttype':'danger'
+            }
+            messages.error(request, "No User Found!")
+            return render(request,"login.html",context)
+        
+    return render(request,'login.html')
+
+
+def logoutUser(request):
+    # return render(request,'index.html')
+    return redirect("/login")
+
+def profile(request):
+    return render(request,"profile.html")
