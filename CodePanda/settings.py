e383829f1ab2a274f0a049eac3d6ecdc7dcf9d2e
AmanVerma0047 <vaman0183@gmail.com>
2024-10-21 22:47:46 +0530

    deploy 11

--- CodePanda/settings.py
+++ CodePanda/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-g#4pi&tu&8j2mjbr^vrnmyl+krmy-&^!kfscuqu*e6r=e7=*3r
 DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app']
-
+#ALLOWED_HOSTS = []
 
 # Application definition
 
