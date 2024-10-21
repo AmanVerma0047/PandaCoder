a09c2c5cb7529d73c75f1816c51ff12d937a3023
AmanVerma0047 <vaman0183@gmail.com>
2024-10-21 21:59:16 +0530

    deploy 8

--- CodePanda/settings.py
+++ CodePanda/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/images/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
